9bf4c0a56b33b7019725759d2f07334b874b9449
tenthyoung <tenthyoung@gmail.com>
2019-05-26 12:49:39 -0700

    got packages

---
+++ routing/apiRoutes.js
@@ -0,0 +1,6 @@
+// Your apiRoutes.js file should contain two routes:
+
+
+
+// A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
+// A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
---
+++ server.js
@@ -0,0 +1,8 @@
+// Dependencies
+// ===========================================================
+var express = require("express");
+
+var app = express();
+var PORT = 3000;
+
+const path = require('path');
