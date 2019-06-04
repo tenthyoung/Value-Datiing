f923ab7d74a85ccaa32c926643349c417ded8553
tenthyoung <tenthyoung@gmail.com>
2019-06-04 16:07:39 -0700

    edited the server

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -2,5 +2,7 @@
 
 
 
-// A GET Route to /survey which should display the survey page.
-// A default, catch-all route that leads to home.html which displays the home page.
+// A GET Route to /survey which should display the 
+// survey page.
+// A default, catch-all route that leads to home.html 
+// which displays the home page.
