e494a413daa2d405bf8becc6b56a056f7c1812ed
tenthyoung <tenthyoung@gmail.com>
2019-06-04 17:53:18 -0700

    still not done with app.js

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,4 +1,31 @@
 $(document).ready(function () {
     $('.sidenav').sidenav();
     $('select').formSelect();
+    $('.modal').modal();
+
+    $("#submit-survey").on("click", (event) => {
+        event.preventDefault();
+
+        let newProfile = {
+            name: $("#name").val().trim(),
+            photo: $('#photo').val().trim(),
+            gender: $("gender").val(),
+            gender: $("interestedIn").val(),
+            scores: [
+                $('#test1').val(),
+                $('#test2').val(),
+                $('#test3').val(),
+                $('#test4').val(),
+                $('#test5').val(),
+                $('#test6').val(),
+                $('#test7').val(),
+                $('#test8').val(),
+                $('#test9').val(),
+                $('#test10').val()
+            ]
+        }
+
+        $.post("api/profile", newProfile)
+        .then
+    })
 });
